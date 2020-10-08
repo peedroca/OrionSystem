2252e565251d429c98c51e67121097cb76245be7
Bruno Assis <bruno.assis6@aluno.unip.br>
2020-10-07 21:22:07 -0300

    Adicionada transição entre telas

--- src/Sirius.Desktop/Models/SmartContractView.cs
+++ src/Sirius.Desktop/Models/SmartContractView.cs
@@ -8,17 +8,6 @@ namespace Sirius.Desktop.Models
 {
     class SmartContractView
     {
-        public class SmartContractViews
-        {
-            public long Id { get; set; }
-            public string title { get; set; }
-            public string description { get; set; }
-            public string terminationCondition { get; set; }
-            public string conclusionCondition { get; set; }
-            public DateTime dueDate { get; set; }
-            public decimal value { get; set; }
-            public string authKey { get; set; }
-
-        }
+        
     }
 }
